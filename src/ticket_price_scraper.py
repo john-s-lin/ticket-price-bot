6859b6dc6fa54209133b1c983aebd98520cad1f9
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2023-05-06 13:40:10 -0400

    feat: make ticket urls collected from txt file

--- src/ticket_price_scraper.py
+++ src/ticket_price_scraper.py
@@ -5,20 +5,8 @@ import re
 import requests
 import sys
 
-from pathlib import Path
 from bs4 import BeautifulSoup
-from utils.utils import LOG_DIR, OUTPUT_DIR, TICKET_INFO_FILE
-
-
-# I just want Twice tickets at the lowest price because actual scalpers are the worst
-TARGET_URLS = [
-    "https://www.ticketmaster.ca/twice-5th-world-tour-ready-to-toronto-ontario-07-02-2023/event/10005E51A7641B24",  # Ticketmaster might block this because it's a bot
-    "https://www.ticketmaster.ca/twice-5th-world-tour-ready-to-toronto-ontario-07-03-2023/event/10005E70271A4DB8",
-    "https://seatgeek.ca/twice-tickets/toronto-canada-scotiabank-arena-4-2023-07-02-7-30-pm/concert/5952228",
-    "https://seatgeek.ca/twice-tickets/toronto-canada-scotiabank-arena-4-2023-07-03-7-30-pm/concert/5976418",
-    "https://www.stubhub.ca/twice-toronto-tickets-7-2-2023/event/151494669/?quantity=2",
-    "https://www.stubhub.ca/twice-toronto-tickets-7-3-2023/event/151600390/?quantity=2",
-]
+from utils.utils import LOG_DIR, OUTPUT_DIR, TICKET_INFO_FILE, TARGET_URLS
 
 
 # Set logging to console
@@ -112,8 +100,13 @@ class TicketInfoScraper:
 
         # Get cheapest ticket info
         cheapest_seat = stats["variants"][0]["stats"]["cheapest_seat_listing"]
-        section = cheapest_seat["section"]
-        ticket_price = cheapest_seat["price_fees"]
+        cheapest_section = cheapest_seat["section"]
+        cheapest_ticket_price = cheapest_seat["price_fees"]
+
+        # Get best deal ticket info
+        best_deal = stats["variants"][0]["stats"]["best_deal_listing"]
+        best_deal_section = best_deal["section"]
+        best_deal_ticket_price = best_deal["price_fees"]
 
         return {
             "date": date,
@@ -122,8 +115,12 @@ class TicketInfoScraper:
             "median_price": median_price,
             "avg_price": avg_price,
             "lowest_price_ticket": {
-                "ticket_price": ticket_price,
-                "section": section,
+                "ticket_price": cheapest_ticket_price,
+                "section": cheapest_section,
+            },
+            "best_deal_ticket": {
+                "ticket_price": best_deal_ticket_price,
+                "section": best_deal_section,
             },
         }
 
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,3 +3,13 @@ from pathlib import Path
 LOG_DIR = Path(__file__).parents[2].joinpath("logs")
 OUTPUT_DIR = Path(__file__).parents[2].joinpath("output")
 TICKET_INFO_FILE = OUTPUT_DIR.joinpath("ticket_info.json")
+INPUT_FILE = Path(__file__).parents[2].joinpath("input/input_url.txt")
+
+
+def get_target_urls_from_file(file_path: str) -> list:
+    with open(file_path, "r") as f:
+        urls = f.readlines()
+    return [url.strip() for url in urls]
+
+
+TARGET_URLS = get_target_urls_from_file(INPUT_FILE)
